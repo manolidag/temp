95bc9676c764eff280980b8ab469577a742fe321
Emanuel Gountoumas <emgounto@e1z3r7p1.42.us.org>
2018-03-03 15:22:11 -0800

    test

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: emgounto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 12:56:22 by emgounto          #+#    #+#             */
-/*   Updated: 2018/02/28 19:25:27 by emgounto         ###   ########.fr       */
+/*   Updated: 2018/03/03 15:22:00 by emgounto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	char str1[n];
-	char str2[n];
+	char str1[n + 1];
+	char str2[n + 1];
 
-	ft_memmove(str1, s1, n);
-	ft_memmove(str2, s2, n);
+	ft_strncpy(str1, s1, n);
+	ft_strncpy(str2, s2, n);
 	str1[n] = '\0';
 	str2[n] = '\0';
 	return (ft_strcmp(str1, str2));
