973f6e4545daa157e0842196fff895fb1a051eb2
Emanuel Gountoumas <emgounto@e1z3r7p1.42.us.org>
2018-03-03 15:37:22 -0800

    test

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: emgounto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 15:45:36 by emgounto          #+#    #+#             */
-/*   Updated: 2018/03/03 15:35:56 by emgounto         ###   ########.fr       */
+/*   Updated: 2018/03/03 15:37:13 by emgounto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ char	*ft_strrchr(const char *s, int c)
 {
 	if (ft_strchr(s, c) == NULL)
 		return (NULL);
-//	while (*s)
-//		s++;
-//	while (*s != (char)c)
-//		s--;
+	while (*s)
+		s++;
+	while (*s != (char)c)
+		s--;
 	return ((char *)s);
 }
