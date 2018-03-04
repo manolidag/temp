50b4ce7806c8c0728a06b929c82f28ba4977de5e
Emanuel Gountoumas <emgounto@e1z2r3p23.42.us.org>
2018-03-04 13:50:44 -0800

    temp

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -3,32 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juochen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: emgounto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/21 13:16:46 by juochen           #+#    #+#             */
-/*   Updated: 2018/02/25 15:09:40 by juochen          ###   ########.fr       */
+/*   Created: 2018/02/20 17:02:29 by emgounto          #+#    #+#             */
+/*   Updated: 2018/02/20 17:46:07 by emgounto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void				*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	*cdst;
-	unsigned char	*csrc;
+	unsigned char			*s1;
+	const unsigned char		*s2;
+	unsigned char			ch;
 
-	cdst = (unsigned char *)dst;
-	csrc = (unsigned char *)src;
-	i = 0;
-	while (i < n)
+	ch = (unsigned char)c;
+	s1 = (unsigned char *)dst;
+	s2 = (const unsigned char *)src;
+	while (n-- > 0)
 	{
-		*cdst++ = *csrc;
-		if (*csrc == (unsigned char)c)
-			return (cdst);
-		else
-			csrc++;
-		i++;
+		if (*s2 == ch)
+		{
+			*s1++ = *s2++;
+			return (s1);
+		}
+		*s1++ = *s2++;
 	}
 	return (NULL);
 }
--- ft_memchr.c
+++ ft_memchr.c
@@ -3,28 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emgounto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: juochen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/21 11:07:55 by emgounto          #+#    #+#             */
-/*   Updated: 2018/02/21 11:57:45 by emgounto         ###   ########.fr       */
+/*   Created: 2018/02/21 14:55:18 by juochen           #+#    #+#             */
+/*   Updated: 2018/02/25 15:10:06 by juochen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void				*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
-	unsigned char	ch;
-	unsigned char	*src;
+	unsigned char	*cs;
 
+	cs = (unsigned char *)s;
 	i = 0;
-	ch = (unsigned char)c;
-	src = (unsigned char *)s;
 	while (i < n)
 	{
-		if (src[i] == ch)
-			return (src + i);
+		if (*cs == (unsigned char)c)
+			return (cs);
+		else
+			cs++;
 		i++;
 	}
 	return (NULL);
