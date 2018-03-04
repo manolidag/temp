7552e9d43b293aba40c63ffaa2ccf17b73d5127e
Emanuel Gountoumas <emgounto@e1z2r3p23.42.us.org>
2018-03-04 14:18:27 -0800

    hey

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juochen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: emgounto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/21 23:17:19 by juochen           #+#    #+#             */
-/*   Updated: 2018/02/27 16:54:46 by juochen          ###   ########.fr       */
+/*   Created: 2018/02/22 12:56:22 by emgounto          #+#    #+#             */
+/*   Updated: 2018/03/03 15:22:00 by emgounto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n)
+int        ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	char	cmp1;
-	char	cmp2;
-	size_t	i;
-
-	i = 0;
-	while (i < n && (*(s1 + i) != 0 && *(s1 + i) == *(s2 + i)))
-		i++;
-	if (i < n)
-	{
-		cmp1 = *(s1 + i);
-		cmp2 = *(s2 + i);
-		return ((unsigned char)cmp1 - (unsigned char)cmp2);
-	}
-	return (0);
+    char    cmp1;
+    char    cmp2;
+    size_t    i;
+    
+    i = 0;
+    while (i < n && (*(s1 + i) != 0 && *(s1 + i) == *(s2 + i)))
+        i++;
+    if (i < n)
+    {
+        cmp1 = *(s1 + i);
+        cmp2 = *(s2 + i);
+        return ((unsigned char)cmp1 - (unsigned char)cmp2);
+    }
+    return (0);
 }
