6f254a9f0833f99419e9d17c1e885b20eb7f55bf
Emanuel Gountoumas <emgounto@e1z2r3p23.42.us.org>
2018-03-04 14:02:34 -0800

    hey

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -3,25 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emgounto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: juochen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/22 11:32:10 by emgounto          #+#    #+#             */
-/*   Updated: 2018/02/22 12:38:51 by emgounto         ###   ########.fr       */
+/*   Created: 2018/02/21 23:11:45 by juochen           #+#    #+#             */
+/*   Updated: 2018/02/28 20:39:17 by juochen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+int		ft_strcmp(const char *s1, const char *s2)
 {
-	unsigned char	*c1;
-	unsigned char	*c2;
-	int				i;
+	int	i;
 
-	c1 = (unsigned char *)s1;
-	c2 = (unsigned char *)s2;
 	i = 0;
-	while (c1[i] && c2[i] && c1[i] == c2[i])
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
-	return (c1[i] - c2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
